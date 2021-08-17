1b75618b457f21b2ea5d84aad74d1eb3a336eee1
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-08-17 16:37:05 -0400

    commit tree 6efd12a8b2f4cb6b6468cc6ef7bc6f414d86d881

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer({toys}) {
+function ToyContainer({toys, deleteToy, increaseLikes}) {
   return (
     <div id="toy-collection">
       {toys.map(toy=><ToyCard
         key={toy.id} 
         toy={toy}
+        deleteToy={deleteToy}
+        increaseLikes = {increaseLikes}
       />)}
     </div>
   );
